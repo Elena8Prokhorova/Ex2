c3170b2a4f644cb056c9b213e0ce43599ac8554f
Elena8Prokhorova <144575984+Elena8Prokhorova@users.noreply.github.com>
2023-10-10 13:53:43 +1000

    Unit-тесты к программе

---
+++ ProgTests1/TestFindReplacement.cs
@@ -0,0 +1,38 @@
+﻿using ex2_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgTests
+{
+    [TestClass]
+    public class FindingReplacement
+    {
+        [TestMethod]
+        public void TestDoesNotHaveReplacement()
+        {
+            string[,] replacement = Prog.GetReplacement();
+            string message = "To where it bent in the undergrowth;";
+            Prog.ItemsOfPermutation expected = new Prog.ItemsOfPermutation();
+            expected.source = null;
+            expected.permutationCounter = 0;
+            expected.itemOfUsedPermutation = 0;
+
+            Prog.ItemsOfPermutation actual = Prog.HasReplacement(message, replacement);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestHasReplacement()
+        {
+            string[,] replacement = Prog.GetReplacement();
+            string message = "I shall be te11ing this with a sigh";
+            Prog.ItemsOfPermutation expected = new Prog.ItemsOfPermutation();
+            expected.source = "l";
+            expected.permutationCounter = 1;
+            expected.itemOfUsedPermutation = 2;
+
+            Prog.ItemsOfPermutation actual = Prog.HasReplacement(message, replacement);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ProgTests1/TestMessageCorrection.cs
@@ -0,0 +1,43 @@
+﻿using ex2_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgTests
+{
+    [TestClass]
+    public class CorrectionMessage
+    {
+        [TestMethod]
+        public void TestWithoutCorrection()
+        {
+            string[,] replacement = Prog.GetReplacement();
+            string message = "To where it bent in the undergrowth;";
+            string expected = "To where it bent in the undergrowth;";
+
+            string actual = Prog.MessageCorrection(message, replacement);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestWithCorrection()
+        {
+            string[,] replacement = Prog.GetReplacement();
+            string message = "Two roads diverged in a d12324344rgg6f5g6gdf2ddjf, and I";
+            string expected = "Two roads diverged in a wood, and I";
+
+            string actual = Prog.MessageCorrection(message,replacement);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestDefaultMessage()
+        {
+            string[,] replacement = Prog.GetReplacement();
+            string message = "An other text";
+            string expected = null;
+
+            string actual = Prog.MessageCorrection(message, replacement);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
